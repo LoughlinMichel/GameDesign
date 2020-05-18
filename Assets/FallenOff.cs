51e886c9320c7c3d2d8efe72451cbcecfed4ab4c
LoughlinMichel <58900051+LoughlinMichel@users.noreply.github.com>
2020-05-18 23:50:18 +1200

    Fifth Commit
    Player will now respawn when player falls off the platform into the "Void", which is just a square sprite which the sprite render has been disabled so that the void isnt visable. The square sprite can be stretched along the level allowing the player to respawn when the player falls off the platform or if a glitch may occurs. The square sprite has a "Box Collider 2D" added "is triggered" turned on so that the "FallenOff" script will work. A "Respawn Point" was added so that the players position will now be instantly moved to the location of the "Respawn Point". A delay may be added to torture the player for dying as well as a respawn screen or game over screen.

---
+++ Assets/FallenOff.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenOff : MonoBehaviour
+{
+    [SerializeField] private Transform player = null;
+    [SerializeField] private Transform respawnPoint = null; //null was added to remove the error.
+
+    private void OnTriggerEnter2D(Collider2D other) //When player collides with void collider then...
+    {
+        player.transform.position = respawnPoint.transform.position; //When player falls off into void player will respawn at the respawn point.
+
+
+    }
+
+}
+
