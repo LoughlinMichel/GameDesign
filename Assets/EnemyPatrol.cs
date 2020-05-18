3f178d3e3c5acdf904cc5607a781e055ea0ffb66
LoughlinMichel <58900051+LoughlinMichel@users.noreply.github.com>
2020-05-18 19:07:59 +1200

    Third Commit
    I thought I would begin my "EnemyPatrol" script allowing the enemys move left and right between a certain area. I added my enemy sprite that being a dino which was 32x32 and scaled it at 4.5. I also added a new test platform to test the "EnemyPatrol" script. I also created an empty transform to detect if the enemy can keep walking or if it must turn around, I put this under the dino as a child file. I made the speed public rather than constant as I plan to have multiple enemies which will move at different speeds depending on what they are and where they are. For the dino I set the speed at 2.5. This all works well.

---
+++ Assets/EnemyPatrol.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    public float speed; //Enemies speed will change depending on enemy public makes it easier to change.
+
+    private bool movingLeft = false; //Enemy will go right until it cant no more.
+
+    public Transform groundDetecton; //Empty will be created to detect ground infront of enemy.
+
+
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime); //Enemy movemnt will equal right * speed * time, meaning it will be constant velocity.
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetecton.position, Vector2.down, 2f); //Raycast will check if there is ground 2 metres below the "groundDetection" which infront of enemy.
+
+        if (groundInfo.collider == false)
+        {
+            if (movingLeft == false) //Enemy will turn around and go left if it cant go right.
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingLeft = true;
+            }
+            else //Otherwise if enemy can go right then it will continue ahead until no ground is detected.
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingLeft = false;
+            }
+        }
+    }
+
+}
