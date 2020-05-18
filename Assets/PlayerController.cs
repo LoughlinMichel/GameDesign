4fd51911f022239ee27992bae38cccab6ebe875c
LoughlinMichel <58900051+LoughlinMichel@users.noreply.github.com>
2020-05-18 18:56:50 +1200

    First Commit
    Player sprite has been added at a size of 32x32 and has been scaled up to 2.25 so its more visable. A "Rigidbody 2D" has been added to the sprite as well as two colliders, a "Box Collider 2D". A "PlayerController" script has also been added with basic movement and jumping. White box sprite was added with "Box Collider 2D" and put onto ground layer to test "PlayerController" script.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 9f;
+
+    const float jumpForce = 15f;
+
+    public LayerMask whatIsGround;
+
+
+    private Rigidbody2D rb;
+    private float inputHorizontal;
+
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //Allowing the player to be controlled easily with "rb".
+    }
+
+    private void Update()
+    {
+
+        inputHorizontal = Input.GetAxisRaw("Horizontal"); //Horrizontal is already known by unity, rather that "if a is pushed then go left".
+
+        rb.velocity = new Vector2(inputHorizontal * speed, rb.velocity.y); //Player new position will equal direction * speed * its movemnt in the sky, allowing player to move while jumping.
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 2f, whatIsGround); //Raycast will check if there is ground 0.2 metres below player.
+        {
+            if (hitInfo.collider != null) //If ground is detected then...
+            {
+                if (Input.GetKeyDown(KeyCode.W)) //When "W" is pressed then player's position will be multiplied upwards by jumpForce.
+                {
+                    rb.velocity = Vector2.up * jumpForce;
+                    Debug.Log("Jump"); //Debug is added to find errors.
+
+                }
+            }
+
+        }
+    }
+}
